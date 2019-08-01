3905f0872b6d928826700602c62f1ffe7621a385
zixun <lvzixun@gmail.com>
2019-08-01 20:17:20 +0800

    update test

--- t.lua
+++ t.lua
@@ -1,6 +1,12 @@
 local print_r = require "print_r"
 local core = require "core"
 
+local function read_file(path)
+    local fd = io.open(path, "r")
+    local s = fd:read("a")
+    fd:close()
+    return s
+end
 
 local source = [[
 for i,v in ipairs(table_name) do
@@ -25,8 +31,15 @@ local bb = require "ui.bb"
 local s1 = 1123
 local s2 = 1123
 local s3 = 1123
+local patt = self:parser_line(line)
+
+local function gen_patt_token_key(patt_token)
+local function complete_at(self, complete_line, complete_index)
+local function complete_at(self, complete_line, complete_index)
+result = complete_at(self, complete_line, complete_index)
 ]]
 
+-- source = read_file("core.lua")
 
 local ctx = core.new_context()
 for line in string.gmatch(source, "[^\r\n]+") do
@@ -39,8 +52,8 @@ print_r(root)
 print("-------------")
 
 
-local function test_complete(complete_line)
-    local result = ctx:complete_at(complete_line)
+local function test_complete(complete_line, line_idx)
+    local result = ctx:complete_at(complete_line, nil, line_idx)
     print("---------- complete_line", complete_line)
     if result then
         print_r(result)
@@ -52,6 +65,8 @@ end
 
 
 test_complete("for")
-test_complete("local s ")
+test_complete("local s")
 test_complete("uns")
-test_complete("local tt")
+test_complete("local func")
+-- test_complete("local sss", 4)
+
